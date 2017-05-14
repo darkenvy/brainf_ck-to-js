be4945a0bb2e100124bde6a85115ba7f6f2ad4ca
darkenvy <darkenvy6@gmail.com>
2017-05-13 21:53:36 -0700

    reflowed code

--- brainfck_std.js
+++ brainfck_std.js
@@ -1,19 +1,13 @@
 module.exports = function Tape(options) {
-  // program, inputFile, arr, options
-  // if (Array.isArray(options) || typeof options !== Object) {
-  //   console.log('conversion ran');
-  //   options = {code: options};
-  // }
   var program    = options.code  || '';
   var inputFile  = options.input || '';
   var reel       = options.reel  || [0];
   var inputIdx   = 0;
-  var current    = 0;
   
+  var current    = 0;
   var cycles     = 0;
   var limit      = options.limit || 0;
   var allowQuit  = options && options.hasOwnProperty('allowQuit') ? false : true;
-  
   var outPrint   = '';
 
   // ------------------------------------------------ //
@@ -22,35 +16,25 @@ module.exports = function Tape(options) {
 
   this.left = function() {
     current -= 1;
-    if (current < 0) {
-      current = 0;
-      console.log('tape indices -1 attempted');
-    }
+    if (current < 0) current = 0;
   }
 
   this.right = function() {
     current += 1;
-    while (reel.length < current + 1) {
-      reel.push(0);
-    }
+    while (reel.length < current + 1) reel.push(0);
   }
 
   this.add = function() {
     reel[current] += 1;
-    if (reel[current] >= 256) {
-      reel[current] = 0 + reel[current];
-    }
+    if (reel[current] >= 256) reel[current] = 0 + reel[current];
   }
 
   this.subtract = function() {
     reel[current] -= 1;
-    if (reel[current] < 0) {
-      reel[current] = 256 + reel[current];
-    }
+    if (reel[current] < 0) reel[current] = 256 + reel[current];
   }
 
   this.output = function() {
-    // console.log(String.fromCharCode(reel[current]));
     outPrint += String.fromCharCode(reel[current]);
   }
 
@@ -58,33 +42,24 @@ module.exports = function Tape(options) {
     // Halt the program safely when out of input
     if (inputIdx > inputFile.length-1 && allowQuit) return null;
     else if (inputIdx > inputFile.length-1) reel[current] = 0;
-
     reel[current] = inputFile[inputIdx].codePointAt(0);
     inputIdx += 1;
   }
 
   this.jumpBack = function(idx) {
     for (var k=idx-1, brackets=0; k>0; k--) {
-      if (program[k] == ']') {
-        brackets += 1;
-      } else if (program[k] === '[' && brackets > 0) {
-        brackets -= 1;
-      } else if (program[k] === '[' && brackets === 0) {
-        return k;
-      }
+      if (program[k] == ']') brackets += 1;
+      else if (program[k] === '[' && brackets > 0  ) brackets -= 1;
+      else if (program[k] === '[' && brackets === 0) return k;
     }
     return null;
   }
 
   this.jumpForward = function(idx) {
     for (var k=idx+1, brackets=0; k<program.length; k++) {
-      if (program[k] == '[') {
-        brackets += 1;
-      } else if (program[k] === ']' && brackets > 0) {
-        brackets -= 1;
-      } else if (program[k] === ']' && brackets === 0) {
-        return k;
-      }
+      if (program[k] == '[') brackets += 1;
+      else if (program[k] === ']' && brackets > 0) brackets -= 1;
+      else if (program[k] === ']' && brackets === 0) return k;
     }
     return null;
   }
